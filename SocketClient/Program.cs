70d47d5c1a99ef40964a84765c1ebe8ca0a61d10
Feitoza <thiagobritofeitoza@gmail.com>
2023-03-28 06:41:30 -0300

    fix: added EOM sign and removed async methods

--- SocketClient/Program.cs
+++ SocketClient/Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿using Sockets;
+
+SocketClient.StartClient();
--- SocketClient/SocketClient.cs
+++ SocketClient/SocketClient.cs
@@ -10,28 +10,40 @@ namespace Sockets
         
         public static void StartClient() 
         {
-            byte[] buffer = new byte[1024];
+            try
+            {
+                byte[] buffer = new byte[1024];
 
-            IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
-            IPAddress ipAdress = ipHostInfo.AddressList[0];
-            IPEndPoint ipEndPoint = new IPEndPoint(ipAdress, 11_000);
+                IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
+                IPAddress ipAdress = ipHostInfo.AddressList[0];
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAdress, 11000);
 
-            Socket client = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                Socket client = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-            client.Connect(ipEndPoint);
+                client.Connect(ipEndPoint);
 
-            Console.WriteLine($"Socket connected to {client.RemoteEndPoint.ToString()}");
+                while(true)
+                {
+                    Console.WriteLine($"Socket connected to {client.RemoteEndPoint.ToString()}");
 
-            byte[] encodedMessage = Encoding.ASCII.GetBytes("Hi! I'm the client");
+                    byte[] encodedMessage = Encoding.ASCII.GetBytes("Hi! I'm the client<|EOM|>");
 
-            _ = client.Send(encodedMessage);
+                    _ = client.Send(encodedMessage);
 
-            int receivedBytes = client.Receive(buffer);
-            string decodedMessage = Encoding.ASCII.GetString(buffer, 0, receivedBytes);
-            Console.WriteLine($"Message received from server: {decodedMessage}");
+                    // int receivedBytes = await client.ReceiveAsync(buffer, SocketFlags.None);
+                    // string decodedMessage = Encoding.ASCII.GetString(buffer, 0, receivedBytes);
+                    // Console.WriteLine($"Message received from server: {decodedMessage}");
+                    break;
+                }
 
-            client.Shutdown(SocketShutdown.Both);
-            client.Close();
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            
         }
         
     }
